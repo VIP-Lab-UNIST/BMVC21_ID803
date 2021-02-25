9f46201d345094f66dda65d064fdc7bf4f54d3d1
bjhan <bjhan@unist.ac.kr>
2021-02-25 18:10:17 +0900

    .

--- auto_draw.py
+++ auto_draw.py
@@ -11,42 +11,45 @@ import time
 from matplotlib.pyplot import figure
 
 
-
 search_dirs = [
     
-    # 'logs/prw/v25/coscale0.1/Feb22_15-14-32',
-    # 'logs/prw/v25/coscale0.1/Feb22_15-14-45',
-    # 'logs/prw/v25/coscale0.1/Feb22_15-14-53',
-    # 'logs/prw/v25/coscale0.1/Feb22_15-15-11',
-    'logs/prw/v25/coscale0.1/Feb22_15-15-19',
+    # # 'logs/prw/v25/coscale0.1/Feb22_15-14-32',
+    # # 'logs/prw/v25/coscale0.1/Feb22_15-14-45',
+    # # 'logs/prw/v25/coscale0.1/Feb22_15-14-53',
+    # # 'logs/prw/v25/coscale0.1/Feb22_15-15-11',
+    # # 'logs/prw/v25/coscale0.1/Feb22_15-15-19',
 
-
-    # 'logs/prw/v25-coAPPieter3/coscale0.1/Feb23_09-16-28',
-    # 'logs/prw/v25-coAPPieter3/coscale0.1/Feb23_09-16-36',
+    # # 'logs/prw/v25-coAPPieter3/coscale0.1/Feb23_09-16-28',
+    # # 'logs/prw/v25-coAPPieter3/coscale0.1/Feb23_09-16-36',
     'logs/prw/v25-coAPPieter3/coscale0.1/Feb23_09-16-42',
-    # 'logs/prw/v25-coAPPieter3/coscale0.1/Feb23_09-16-46',
-    # 'logs/prw/v25-coAPPieter3/coscale0.1/Feb23_09-16-50',
+    # # 'logs/prw/v25-coAPPieter3/coscale0.1/Feb23_09-16-46',
+    # # 'logs/prw/v25-coAPPieter3/coscale0.1/Feb23_09-16-50',
 
-    # 'logs/prw/v25-woCoapp/coscale0.1/Feb22_21-13-07',
-    'logs/prw/v25-woCoapp/coscale0.1/Feb22_21-13-16',
-    # 'logs/prw/v25-woCoapp/coscale0.1/Feb22_21-13-25',
+    # # 'logs/prw/v25-woCoapp/coscale0.1/Feb22_21-13-07',
+    # 'logs/prw/v25-woCoapp/coscale0.1/Feb22_21-13-16',
+    # # 'logs/prw/v25-woCoapp/coscale0.1/Feb22_21-13-25',
 
-    'logs/prw/v25-noCycle/coscale0.1/Feb23_13-09-39',
-    # 'logs/prw/v25-noCycle/coscale0.1/Feb23_16-54-10',
-    # 'logs/prw/v25-noCycle/coscale0.1/Feb23_16-54-23',
+    # # 'logs/prw/v25-coAppIter3-noCycle/coscale0.1/Feb24_09-37-21',
+    # 'logs/prw/v25-coAppIter3-noCycle/coscale0.1/Feb24_09-37-30',
+    # # 'logs/prw/v25-coAppIter3-noCycle/coscale0.1/Feb24_09-37-37',
+
+    # # 'logs/prw/v25-noCycle/coscale0.1/Feb23_13-09-39',
+    # # # 'logs/prw/v25-noCycle/coscale0.1/Feb23_16-54-10',
+    # # # 'logs/prw/v25-noCycle/coscale0.1/Feb23_16-54-23',
     
-    'logs/prw/v25-noCycle-noCoap/coscale0.1/Feb24_09-32-39',
-    'logs/prw/v25-noCycle-noCoap/coscale0.1/Feb24_09-32-51',
+    # 'logs/prw/v25-noCycle-noCoap/coscale0.1/Feb24_09-32-39',
+    # # 'logs/prw/v25-noCycle-noCoap/coscale0.1/Feb24_09-32-51',
+    # # 'logs/prw/v25-noCycle-noCoap/coscale0.1/Feb24_17-39-18',
 
-    'logs/prw/v25-coAppIter3-noCycle/coscale0.1/Feb24_09-37-21',
-    'logs/prw/v25-coAppIter3-noCycle/coscale0.1/Feb24_09-37-30',
-    'logs/prw/v25-coAppIter3-noCycle/coscale0.1/Feb24_09-37-37',
+    'logs/prw/v32/pathTemp10/Feb24_21-07-13',
+    # 'logs/prw/v32/pathTemp1/Feb24_21-08-01',
+    # 'logs/prw/v32/pathTemp0.1/Feb24_21-08-49',
 
- 
+    # 'logs/prw/v33/pathTemp10/Feb25_14-01-51',
+    # 'logs/prw/v33/pathTemp1/Feb25_14-02-08',
+    # 'logs/prw/v33/pathTemp0.1/Feb25_14-02-23',
 
 ]   
-
-
 
 # search_dirs = [ 
 #     'logs/prw/v14/coscale0.4-weight0.1/Feb10_13-23-00',
--- auto_eval.py
+++ auto_eval.py
@@ -7,6 +7,7 @@ abspath = os.path.abspath(__file__)
 dname = os.path.dirname(abspath)
 os.chdir(dname)
 
+
 search_dirs = [
     
     # 'logs/cuhk/v25/coscale0.1/Feb20_21-27-00',
@@ -17,7 +18,6 @@ search_dirs = [
     'logs/prw/v25/coscale0.1/Feb22_15-14-53',
     'logs/prw/v25/coscale0.1/Feb22_15-15-11',
     'logs/prw/v25/coscale0.1/Feb22_15-15-19',
-
 
     'logs/prw/v25-coAPPieter3/coscale0.1/Feb23_09-16-28',
     'logs/prw/v25-coAPPieter3/coscale0.1/Feb23_09-16-36',
@@ -32,14 +32,26 @@ search_dirs = [
     'logs/prw/v25-noCycle/coscale0.1/Feb23_13-09-39',
     'logs/prw/v25-noCycle/coscale0.1/Feb23_16-54-10',
     'logs/prw/v25-noCycle/coscale0.1/Feb23_16-54-23',
+
     'logs/prw/v25-noCycle-noCoap/coscale0.1/Feb23_16-55-04',
     'logs/prw/v25-noCycle-noCoap/coscale0.1/Feb24_09-32-39',
-    'logs/prw/v25-noCycle-noCoap/coscale0.1/Feb24_09-32-51',
+    'logs/prw/v25-noCycle-noCoap/coscale0.1/Feb24_17-39-18',
     
     'logs/prw/v25-coAppIter3-noCycle/coscale0.1/Feb24_09-37-21',
     'logs/prw/v25-coAppIter3-noCycle/coscale0.1/Feb24_09-37-30',
     'logs/prw/v25-coAppIter3-noCycle/coscale0.1/Feb24_09-37-37',
+
+    'logs/prw/v32/pathTemp10/Feb24_21-07-13',
+    'logs/prw/v32/pathTemp1/Feb24_21-08-01',
+    'logs/prw/v32/pathTemp0.1/Feb24_21-08-49',
+
+    'logs/prw/v33/pathTemp10/Feb25_14-01-51',
+    'logs/prw/v33/pathTemp1/Feb25_14-02-08',
+    'logs/prw/v33/pathTemp0.1/Feb25_14-02-23',
+
 ]   
+
+
 
 
 
